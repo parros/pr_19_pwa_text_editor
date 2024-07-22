23b69cee17be2dfea1134c3767b11765a1656932
unknown <parkerosemeyer@gmail.com>
2024-07-22 16:15:12 -0500

    Added database put and get js and install button js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,32 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Add logic to a method that accepts some content and adds it to the database
+const putDb = async textData => {
+  const db = await openDB('jate', 1)
+
+  const readWriteDb = await db.transaction(objectStoreNames, 'readwrite')
+
+  const store = await readWriteDb.objectStore(objectStoreNames)
+
+  const result = await store.put(textData)
+  return result
+}
+
 export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
+// Add logic for a method that gets all the content from the database
+const getDb = async query => {
+  const db = await openDB('jate', 1)
+
+  const readDb = await db.transaction(objectStoreNames, 'readonly')
+
+  const store = await readDb.objectStore(objectStoreNames)
+
+  const result = await store.getAll()
+  return result
+}
+
 export const getDb = async () => console.error('getDb not implemented');
 
 initdb();
